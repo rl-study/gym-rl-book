fc059940b3f5bf93d9798fa54baf4150da229d0d
void-main <voidmain1313113@gmail.com>
2019-02-21 21:06:13 +0800

    add url and author meta

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@ setup(name='gym_rl_book',
       install_requires=['gym', 'numpy', 'tabulate'],
       packages=['gym_rl_book'],
       long_description=open('README.md').read(),
+      url='https://github.com/void-main/gym-rl-book',
+      author='void-main',
+      author_email='voidmain1313113@gmail.com',
 )
