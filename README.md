1099e5c01d21399cb1981d06ea7dc4394fb302e1
void-main <voidmain1313113@gmail.com>
2019-02-21 21:04:20 +0800

    add README for release

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gym RL Book
+OpenAI gym environments for Reinforcement Learning an Introduction, 2nd Edition
+
+## TicTacToe-v0
+> TicTacToe from Chapter 1.
+A tic-tac-toe game, where players take turns to place stones on a 3x3 board.
+
--- setup.py
+++ setup.py
@@ -2,5 +2,7 @@ from setuptools import setup
 
 setup(name='gym_rl_book',
       version='0.0.1',
-      install_requires=['gym', 'numpy', 'tabulate']
+      install_requires=['gym', 'numpy', 'tabulate'],
+      packages=['gym_rl_book'],
+      long_description=open('README.md').read(),
 )
