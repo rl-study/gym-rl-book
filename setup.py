adc2e463a566989eb5a5e5dece04b99aa4b8c7e9
void-main <voidmain1313113@gmail.com>
2019-02-22 09:56:29 +0800

    fix packages

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(name='gym_rl_book',
       version='0.0.1',
       install_requires=['gym', 'numpy', 'tabulate'],
-      packages=['gym_rl_book'],
       long_description=open('README.md').read(),
       url='https://github.com/void-main/gym-rl-book',
       author='void-main',
