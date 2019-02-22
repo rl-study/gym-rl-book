5d1d5ebf3064702caa4def44c3306b2042afa5cc
void-main <voidmain1313113@gmail.com>
2019-02-22 09:59:18 +0800

    bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gym_rl_book',
-      version='0.0.1',
+      version='0.0.2',
       install_requires=['gym', 'numpy', 'tabulate'],
       long_description=open('README.md').read(),
       url='https://github.com/void-main/gym-rl-book',
