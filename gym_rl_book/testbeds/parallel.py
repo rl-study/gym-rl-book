379849c77445e8dba9e775d590997b3488e444f2
void-main <voidmain1313113@gmail.com>
2019-04-01 10:29:03 +0800

    record values to parameters

--- gym_rl_book/testbeds/parallel.py
+++ gym_rl_book/testbeds/parallel.py
@@ -1,9 +1,9 @@
 import psutil
-import collections
+from collections import defaultdict, namedtuple
 import multiprocessing as mul
 import matplotlib.pyplot as plt
 
-TaskSpec = collections.namedtuple('TaskSpec',
+TaskSpec = namedtuple('TaskSpec',
                                   [
                                       'env', 'agent', 'parameter',
                                       'episode_count', 'steps_per_episode',
@@ -14,7 +14,7 @@ TaskSpec = collections.namedtuple('TaskSpec',
 class PTestbed:
     def __init__(self, num_worker=psutil.cpu_count()):
         self.num_worker = num_worker
-        self._results = {}
+        self._results = defaultdict(dict)
         self._parameter_sets = []
 
     @staticmethod
@@ -38,7 +38,7 @@ class PTestbed:
                     avg = (avg * (count - 1) + reward) / count
             if (i + 1) % 100 == 0:
                 print("[Agent {}] Episode: {}".format(key, i + 1))
-        return key, avg
+        return key, p, avg
 
     @staticmethod
     def _build_tasks(env_maker, agents_maker, parameter_sets, episode_count, steps_per_episode, stats_start_steps):
@@ -54,7 +54,7 @@ class PTestbed:
         return tasks
 
     def run(self, env_maker, agents_maker, parameter_sets, episode_count, steps_per_episode, stats_start_steps):
-        self._results = {}
+        self._results = defaultdict(dict)
         self._parameter_sets = parameter_sets
         tasks = PTestbed._build_tasks(env_maker,
                                       agents_maker,
@@ -66,8 +66,8 @@ class PTestbed:
         with mul.Pool(processes=self.num_worker) as pool:
             results = pool.map(PTestbed._run_agent, tasks)
 
-        for key, avg in results:
-            self._results[key] = avg
+        for key, p, avg in results:
+            self._results[key][p] = avg
 
     def plot(self):
         xs = [x for x in range(len(self._parameter_sets))]
@@ -80,7 +80,8 @@ class PTestbed:
 
         color_idx = 0
         for k in self._results.keys():
-            ax1.plot(xs, self._results[k], colors[color_idx], label=k)
+            ys = [self._results[k][x] for x in xs]
+            ax1.plot(xs, ys, colors[color_idx], label=k)
             color_idx += 1
             color_idx %= len(colors)
 
