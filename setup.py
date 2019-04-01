6b4f2f1a741b17aadf3190ec39efa2ab3425791d
void-main <voidmain1313113@gmail.com>
2019-04-01 11:04:45 +0800

    bump version to 0.0.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='gym_rl_book',
-                 version='0.0.6',
+                 version='0.0.7',
                  install_requires=['gym', 'numpy', 'tabulate', 'matplotlib'],
                  packages=setuptools.find_packages(),
                  long_description=open('README.md').read(),
